ccf89d4ceeab816d5002550135a63715b490d12d
Platide <andresluna1717@gmail.com>
2024-11-12 13:30:39 -0600

    Add files via upload

---
+++ Copiarypegar.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package copiarypegar;
+import java.awt.*;
+import javax.swing.*;
+/**
+ *
+ * @author Alumno
+ */
+public class Copiarypegar extends JFrame{
+
+    private JLabel labelpegar = new JLabel("Texto Copiado");
+    private JLabel labeltitulo = new JLabel("Texto Copiado");
+    private JTextField textcopiar = new JTextField();
+    public Copiarypegar(){
+        
+        setTitle("Copiar Pegar");
+        setSize(350, 300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setLayout(new FlowLayout(FlowLayout.LEFT,10,20));
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Copiarypegar ventana= new Copiarypegar();
+        ventana.setVisible(true);
+    }
+    
+}
